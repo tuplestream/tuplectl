bb41b791b6507cec4a4c0cb45c529a926eeb0450
Chris Mowforth <chris@mowforth.com>
2021-02-10 11:03:18 -0300

    add -s and -w flags, cuts down on binary size a little

--- build.bash
+++ build.bash
@@ -13,10 +13,10 @@ if [[ " $@ " =~ " -release" ]]; then
   mkdir -p bin
 
   # go vet
-  GOOS=linux GOARCH=amd64 go build -ldflags "-X main.Commit=$COMMIT -X main.Version=$VERSION -X 'main.BuildDate=$BUILD_DATE' -X main.DefaultClientID=$AUTH0_CLIENT_ID -X main.DefaultTenantURL=$AUTH0_TENANT_URL" -a -o bin/tuplectl-linux-amd64 .
-  GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.Commit=$COMMIT -X main.Version=$VERSION -X 'main.BuildDate=$BUILD_DATE' -X main.DefaultClientID=$AUTH0_CLIENT_ID -X main.DefaultTenantURL=$AUTH0_TENANT_URL" -a -o bin/tuplectl-darwin-amd64 .
+  GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X main.Commit=$COMMIT -X main.Version=$VERSION -X 'main.BuildDate=$BUILD_DATE' -X main.DefaultClientID=$AUTH0_CLIENT_ID -X main.DefaultTenantURL=$AUTH0_TENANT_URL" -a -o bin/tuplectl-linux-amd64 .
+  GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -X main.Commit=$COMMIT -X main.Version=$VERSION -X 'main.BuildDate=$BUILD_DATE' -X main.DefaultClientID=$AUTH0_CLIENT_ID -X main.DefaultTenantURL=$AUTH0_TENANT_URL" -a -o bin/tuplectl-darwin-amd64 .
 else
   VERSION="$VERSION-$(hostname -f)-local"
   COMMIT="$COMMIT-$(git rev-parse --abbrev-ref HEAD)"
-  go build -ldflags "-X main.Commit=$COMMIT -X 'main.Version=$VERSION' -X 'main.BuildDate=$BUILD_DATE'  -X main.DefaultClientID=$AUTH0_CLIENT_ID -X main.DefaultTenantURL=$AUTH0_TENANT_URL" -a -o tuplectl .
+  go build -ldflags "-s -w -X main.Commit=$COMMIT -X 'main.Version=$VERSION' -X 'main.BuildDate=$BUILD_DATE'  -X main.DefaultClientID=$AUTH0_CLIENT_ID -X main.DefaultTenantURL=$AUTH0_TENANT_URL" -a -o tuplectl .
 fi
